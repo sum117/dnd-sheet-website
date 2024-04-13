9d0a0cb19e0f847078b35f5bff4066a59cc43af4
sum117 <jvcaliman@hotmail.com>
2024-04-13 06:28:34 -0300

    feat: add all classes icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,25 @@
-import BardIconComponent from "@/components/svg/Bard";
+import BardIconComponent from "@/components/svg/BardIcon";
 import AppLogo from "./svg/AppLogo";
-import SayuriRectangle from "./svg/SayuriRectangle";
+import SayuriRectangle from "./svg/SayuriRectangleIcon";
 
 export function Header() {
   return (
     <header className="overflow-hidden text-foreground">
       <nav className="bg-[#1E1E2E]">
         <ul className="flex">
-          <li className="hover:bg-accent flex cursor-pointer items-center px-5 tracking-widest">
+          <li className="flex cursor-pointer items-center px-5 tracking-widest hover:bg-accent">
             <div className="relative mr-4 flex h-full items-center">
               <BardIconComponent className="absolute -bottom-6 -left-7 h-[6.65rem] w-[6.65rem] fill-foreground opacity-[0.08]" />
               <BardIconComponent className="h-12 w-12 fill-foreground" />
             </div>
             <span className="mr-2 text-2xl">Sayumi</span>
-            <span className="text-foreground/40 translate-y-[0.125rem]">The Bard</span>
+            <span className="translate-y-[0.125rem] text-foreground/40">The Bard</span>
           </li>
           <span className="my-auto">
             <SayuriRectangle className="fill-foreground/40" />
           </span>
-          <li className="hover:bg-accent flex cursor-pointer items-center px-5 text-2xl tracking-widest">Background</li>
-          <li className="hover:bg-accent flex cursor-pointer items-center px-5 text-2xl tracking-widest">Equipment</li>
+          <li className="flex cursor-pointer items-center px-5 text-2xl tracking-widest hover:bg-accent">Background</li>
+          <li className="flex cursor-pointer items-center px-5 text-2xl tracking-widest hover:bg-accent">Equipment</li>
           <AppLogo className="mx-4 my-3 ml-auto h-10 w-10" />
         </ul>
       </nav>
--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,18 +1,18 @@
-import ChestArmor from "@/components/svg/ChestArmor";
-import WarriorHelmet from "@/components/svg/WarriorHelmet";
+import ChestArmorIcon from "@/components/svg/ChestArmorIcon";
+import WarriorHelmetIcon from "@/components/svg/WarriorHelmetIcon";
 import { LucideIcon, Sword, Target, Weight } from "lucide-react";
 
 export const itemSlots = [
-  { id: "head", name: "Head", description: "A head slot item", Icon: WarriorHelmet, accepts: ["armor"] },
-  { id: "chest", name: "Chest", description: "A chest slot item", Icon: ChestArmor, accepts: ["armor"] },
-  { id: "ringOne", name: "Ring #1", description: "A ring slot item", Icon: ChestArmor, accepts: ["armor", "adventuring-gear"] },
-  { id: "ringTwo", name: "Ring #2", description: "A ring slot item", Icon: ChestArmor, accepts: ["armor", "adventuring-gear"] },
-  { id: "feet", name: "Feet", description: "A feet slot item", Icon: ChestArmor, accepts: ["armor"] },
-  { id: "amulet", name: "Amulet", description: "An amulet slot item", Icon: ChestArmor, accepts: ["armor", "adventuring-gear"] },
-  { id: "hands", name: "Hands", description: "A hands slot item", Icon: ChestArmor, accepts: ["armor"] },
-  { id: "back", name: "Back", description: "A back slot item", Icon: ChestArmor, accepts: ["armor"] },
-  { id: "leftArm", name: "Left Arm", description: "A left hand slot item", Icon: ChestArmor, accepts: ["weapon"] },
-  { id: "rightArm", name: "Right Arm", description: "A right hand slot item", Icon: ChestArmor, accepts: ["weapon"] },
+  { id: "head", name: "Head", description: "A head slot item", Icon: WarriorHelmetIcon, accepts: ["armor"] },
+  { id: "chest", name: "Chest", description: "A chest slot item", Icon: ChestArmorIcon, accepts: ["armor"] },
+  { id: "ringOne", name: "Ring #1", description: "A ring slot item", Icon: ChestArmorIcon, accepts: ["armor", "adventuring-gear"] },
+  { id: "ringTwo", name: "Ring #2", description: "A ring slot item", Icon: ChestArmorIcon, accepts: ["armor", "adventuring-gear"] },
+  { id: "feet", name: "Feet", description: "A feet slot item", Icon: ChestArmorIcon, accepts: ["armor"] },
+  { id: "amulet", name: "Amulet", description: "An amulet slot item", Icon: ChestArmorIcon, accepts: ["armor", "adventuring-gear"] },
+  { id: "hands", name: "Hands", description: "A hands slot item", Icon: ChestArmorIcon, accepts: ["armor"] },
+  { id: "back", name: "Back", description: "A back slot item", Icon: ChestArmorIcon, accepts: ["armor"] },
+  { id: "leftArm", name: "Left Arm", description: "A left hand slot item", Icon: ChestArmorIcon, accepts: ["weapon"] },
+  { id: "rightArm", name: "Right Arm", description: "A right hand slot item", Icon: ChestArmorIcon, accepts: ["weapon"] },
 ];
 export type ItemSlot = (typeof itemSlots)[number];
 
